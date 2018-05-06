0af3aca576c8a53e95e8f43ef4c3cfb94d6a12a0
mvbreddy <mvbr.iitk@gmail.com>
2018-05-06 21:05:32 +0530

    Update KMeans_ Model_MLA_Combined.R

--- KMeans_ Model_MLA_Combined.R
+++ KMeans_ Model_MLA_Combined.R
@@ -13,13 +13,12 @@
 
 setwd("C:/Users/vijay.bhaskar/Desktop/MLA LADS/")
 
-dataset <- read.csv('Combined_data_v1.csv')
+dataset <- read.csv('Combined_data_v1.csv')     #This csv is the output of Data Prep Code
 
 
 library(tm)
 
-
-
+# Create a corpus
 
 corpus <- Corpus(VectorSource(dataset$item_description))
 
@@ -31,37 +30,35 @@ corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, removeWords, stopwords('english'))
 
+#Create a dtm and form a TF-IDF matrix
+
 mat <- DocumentTermMatrix(corpus)
 mat
-
 
 mat4 <- weightTfIdf(mat)
 mat4 <- as.matrix(mat4)
 mat4
 
+#Function for Euclidean distance calculation
 
 norm_eucl <- function(m)
   m/apply(m,1,function(x) sum(x^2)^.5)
+          
 mat_norm <- norm_eucl(mat4)
 
+#Perform K-Means Clustering
 
 set.seed(5)
 k <- 10
-kmeansResult <- kmeansruns(mat_norm, k)
+kmeansResult <- kmeans(mat_norm, k,iter=100)
 
 
 table(kmeansResult$cluster)
 
-
-
-
-write.csv(output,"kmeans_output.csv")
-
-
+#Output
+          
 output1 <- cbind(dataset[,c(2,5:6)],kmeansResult$cluster)
-
-
-write.csv(output1,"kmeans_output1.csv")
+write.csv(output1,"output_combined.csv")
 
 
 
